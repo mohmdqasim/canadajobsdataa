6497a36df2b700d41b23d88bdaaf701743613d5a
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-03 03:44:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pathlib import Path
 
 
-st.set_page_config(page_title="Job Listings", layout="wide")
+st.set_page_config(page_title="Job Listings!!", layout="wide")
 st.title("Scrapped Data")
 file = Path(__file__).parent / "JobTitles.csv"
 df = pd.read_csv(file)
