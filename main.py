94dc9bbeae97dfb6538822a0f7e7c7f540a89378
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-11 18:49:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 st.set_page_config(page_title="Job Listings!!", layout="wide")
-st.title("Scrapped Data!!")
+st.title("Job Portal!!")
 file = Path(__file__).parent / "JobTitles.csv"
 df = pd.read_csv(file)
 
