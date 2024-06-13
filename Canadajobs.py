fd771a3afe84391a6135f17d1775b2813c7791f1
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-13 16:26:14 +0500

    update

--- Canadajobs.py
+++ Canadajobs.py
@@ -11,15 +11,10 @@ def canada():
     # Streamlit application
     st.title('Job Listings!!')
     # Display the list of markdown files in a sidebar
-    st.header("Select a Tag")
+    st.write("Select a Tag")
     selected_file = st.selectbox('', markdown_files)
     # Display the content of the selected markdown file
     if selected_file:
-        st.subheader(selected_file)
         file_path = os.path.join(directory, selected_file)
         df = pd.read_csv(file_path)
         st.dataframe(df, height=1500)
-
-
-
-
