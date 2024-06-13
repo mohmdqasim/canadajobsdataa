080e6b9447df6052436022dd8b6c939f096092c4
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-13 15:47:08 +0500

    Updated directory style

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 st.set_page_config(page_title="Job Listings!!", layout="wide")
 st.title("Job Portal!!")
-file = Path(__file__).parent / "JobTitles.csv"
+file = Path(__file__).parent / "Canadjobs Website/IT.csv"
 df = pd.read_csv(file)
 
 st.dataframe(df, height=1500)
