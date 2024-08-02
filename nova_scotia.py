fd4f4551bafbe25b397ba2c24a456b359c0adde0
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-08-02 19:23:19 +0500

    search key

--- nova_scotia.py
+++ nova_scotia.py
@@ -1,9 +1,10 @@
 import os
 import streamlit as st
 import pandas as pd
+from df_global_search import DataFrameSearch
 
 
-def nova():
+def nbj():
     
     # Directory containing the markdown files
     directory = 'nova_scotia'
@@ -20,5 +21,24 @@ def nova():
     # Display the content of the selected markdown file
     if selected_file:
         file_path = os.path.join(directory, selected_file+".csv")
+        search_bar_columns = st.columns((2, 1, 0.5, 0.75, 1))
+        with search_bar_columns[1]:
+            search_text = st.text_input(
+                "Search", label_visibility="collapsed", placeholder="Search Text"
+            )
+        with search_bar_columns[2]:
+            is_regex = st.toggle("Regex", value=False)
+        with search_bar_columns[3]:
+            case_sensitive = st.toggle("Case Sensitive", value=False)
+        with search_bar_columns[4]:
+            highlight_match = st.toggle("Highlight Matching Cells", value=True)
+        
         df = pd.read_csv(file_path)
-        st.dataframe(df, height=700)
+        with DataFrameSearch(
+            dataframe=df,
+            text_search=search_text,
+            case_sensitive=case_sensitive,
+            regex_search=is_regex,
+            highlight_matches=highlight_match,
+        ) as df:
+            st.dataframe(data=df, use_container_width=True, hide_index=True)
